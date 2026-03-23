2ae1df83359c77f75a5a0e372a58dc50bb116105
salna shareef <salnasayuvm7@gmail.com>
2026-03-24 01:45:12 +0400

    add login system mini project  in c#

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace LoginSystemMiniProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string correctUsername = "admin";
+            string correctPassword = "1234";
+            int attempts = 3;
+
+            Console.WriteLine("==== LOGIN SYSTEM ====");
+            while (attempts > 0)
+            {
+                Console.WriteLine("ENTER USERNAME: ");
+                string username = Console.ReadLine();
+                Console.WriteLine("ENTER PASSWORD:");
+                string password = Console.ReadLine();
+                if (username == correctUsername && password == correctPassword)
+                {
+                    Console.WriteLine("LOGIN SUCCESSFULL !!! WELCOME " + username);
+                    break;
+                }
+                else
+                {
+                    attempts--;
+                    Console.WriteLine($"LOGIN FAILED !!! ATTEMPTS LEFT : {attempts}");
+                }
+                if (attempts == 0)
+                {
+                    Console.WriteLine("ALL ATTEMPTS USED ...... EXITING PROGRAM");
+                }
+
+
+            }
+            Console.WriteLine("PRESS ANY KEY TO EXIT .......");
+            Console.ReadKey();
+        }
+    }
+}
